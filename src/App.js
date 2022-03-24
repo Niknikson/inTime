84c6d3038698a34570671b719f6de874a65d95bc
Niknikson <niknikson34@gmail.com>
2022-03-24 20:00:55 +0300

    clear project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import Main from './Components/Main/Main'
-import { BrowserRouter } from 'react-router-dom';
-import s from "./index.module.css";
-
 
 const App = () => {
-
-  return (
-    <BrowserRouter>
-      <div className={s.container}>
-        <Main />
-      </div>
-    </BrowserRouter>
-  );
+    return <div>inTime</div>;
 };
 
 export default App;
