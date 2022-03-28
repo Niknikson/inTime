fd7ba05ee748a231514bb76f6f8ff665a670aea7
Tolik9924 <76564089+Tolik9924@users.noreply.github.com>
2022-03-28 15:48:33 +0300

    added: Button component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,21 @@ const App = () => {
     return (
         <div>
             <h1>inTime</h1>
-            <Button />
+            <Button
+                border="3px solid #218294"
+                backgroundColor="#fff"
+                color="#218294"
+                height="60px"
+                onClick={() => console.log("You clicked on the pink circle!")}
+                radius="none"
+                width="250px"
+                children="Button"
+                fontSize="30px" 
+                textDecoration="none"
+                display="block"
+                lineHeight="60px"
+                fontFamily="Arial, sans-serif"
+            />
         </div>
     );
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, {FC} from 'react';
 
 import style from "./Button.module.css";
 
-const Button = () => {
+interface Props {
+    border: string;
+    backgroundColor: string;
+    color: string;
+    children?: React.ReactNode;
+    height: string;
+    onClick: () => void;
+    radius: string;
+    width: string;
+    fontSize: string;
+    textDecoration: string;
+    display: string;
+    lineHeight: string;
+    fontFamily: string;
+}
+
+const Button: FC<Props> = ({
+    border,
+    backgroundColor,
+    color,
+    children,
+    height,
+    onClick,
+    radius,
+    width,
+    fontSize,
+    textDecoration,
+    display,
+    lineHeight,
+    fontFamily
+}) => {
     return(
         <div>
-            <button type="button" className={style.button}>Button</button>
+            <button onClick={onClick} 
+                    style={{
+                        backgroundColor,
+                        color,
+                        border,
+                        borderRadius: radius,
+                        height,
+                        width,
+                        fontSize,
+                        textDecoration,
+                        display,
+                        lineHeight,
+                        fontFamily
+                    }} 
+                    className={style.button}
+            >
+                    {children}
+            </button>
         </div>
     );
 }
